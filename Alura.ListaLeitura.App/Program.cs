dd589f000b4964125f07efac45e88f5a090bc8a3
João Paulo Pacheco da Silva <joaopaulo.pacheco@outlook.com.br>
2021-11-01 21:11:10 -0300

    Cleaned configure

--- Alura.ListaLeitura.App/Program.cs
+++ Alura.ListaLeitura.App/Program.cs
@@ -1,16 +1,11 @@
-﻿using Alura.ListaLeitura.App.Negocio;
-using Alura.ListaLeitura.App.Repositorio;
-using Microsoft.AspNetCore.Hosting;
-using System;
+﻿using Microsoft.AspNetCore.Hosting;
 
 namespace Alura.ListaLeitura.App
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            var _repo = new LivroRepositorioCSV();
-
             IWebHost host = new WebHostBuilder()
                 .UseKestrel()
                 .UseStartup<Startup>()
@@ -18,14 +13,7 @@ namespace Alura.ListaLeitura.App
 
             host.Run();
 
-            //ImprimeLista(_repo.ParaLer);
-            //ImprimeLista(_repo.Lendo);
-            //ImprimeLista(_repo.Lidos);
         }
 
-        static void ImprimeLista(ListaDeLeitura lista)
-        {
-            Console.WriteLine(lista);
-        }
     }
 }
