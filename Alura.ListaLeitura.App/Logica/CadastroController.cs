063f09b7e5aaaed12a22794b69fd74fffeb57ce4
João Paulo Pacheco da Silva <joaopaulo.pacheco@outlook.com.br>
2021-11-04 21:41:01 -0300

    finished

---
+++ Alura.ListaLeitura.App/Logica/CadastroController.cs
@@ -0,0 +1,27 @@
+﻿using Alura.ListaLeitura.App.Negocio;
+using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Alura.ListaLeitura.App.Logica
+{
+    public class CadastroController
+    {
+
+
+        public IActionResult ExibeFormulario()
+        {
+             return new ViewResult { ViewName = "formulario"};
+            
+        }
+
+        public string NovoLivro(Livro livro)
+        {
+            var repo = new LivroRepositorioCSV();
+            repo.Incluir(livro);
+
+            return "O livro foi adicionado com sucesso";
+
+        }
+
+    }
+}
